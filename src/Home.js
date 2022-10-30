202bfbc4bab9e7570be91d8d477321f99f056a9c
Adarsha A <adarshaadi1997@gmail.com>
2022-10-30 18:38:26 +0530

    Sevices and tusted brands components

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
+import Services from './components/Services';
+import Trusted from './components/Trusted';
 
 const Home = () => {
     const data = {
         name: "My Store"
     };
   return (
+    <>
     <HeroSection  myData={data}/>
+    <Services />
+    <Trusted />
+    </>
   )
 }
 export default Home;
