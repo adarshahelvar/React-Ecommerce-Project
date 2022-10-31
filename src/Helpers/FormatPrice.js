e8b0321c270bac1add9774e12b49e4d5f399adef
Adarsha A <adarshaadi1997@gmail.com>
2022-10-31 18:49:34 +0530

    INTL number() format to rupees fromat

---
+++ src/Helpers/FormatPrice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormatPrice = ({price}) => {
+  return Intl.NumberFormat('en-IN',{
+    style: 'currency',
+    currency: 'INR',
+    minimumFractionDigits:2,
+  }).format( price / 100 );                   //Rupees = price/100 because in beginning price will be in pisa (1rs=100pisa)
+}
+
+export default FormatPrice;
