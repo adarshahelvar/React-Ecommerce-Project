2f0ca60b33012dc5d74a4c41da8e3654951bfeb0
Adarsha A <adarshaadi1997@gmail.com>
2022-11-01 22:36:30 +0530

    Traditional sorting method

--- src/Context/filter_context.js
+++ src/Context/filter_context.js
@@ -7,6 +7,7 @@ const initialState =  {
     filter_products: [],
     all_products: [],
     grid_view: true,
+    sorting_value:"lowest",
 }
 
 export const FilterContextProvider = ({ children }) => {
@@ -24,11 +25,22 @@ export const FilterContextProvider = ({ children }) => {
     return dispatch({ type: "SET_LIST_VIEW" });
   };
 
+  // sorting function 
+
+  const sorting =() =>{
+    dispatch({ type:"GET_SORT_VALUE" });
+  };
+
+  // To sort the product 
+  useEffect(()=>{
+    dispatch({ type:"SORTING_PRODUCTS", payload:products });
+  },[state.sorting_value])
+
 useEffect(()=>{
   dispatch({type:"LOAD_FILTER_PRODUCTS", payload: products})
 },[products])
 
-    return <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
+    return <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting }}>
         {children}
     </FilterContext.Provider>
 }
