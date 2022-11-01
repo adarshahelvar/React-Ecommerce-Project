8406932c971fd890d57ab4ab0349cc57462e3b0b
Adarsha A <adarshaadi1997@gmail.com>
2022-11-01 20:40:36 +0530

    Grid and List View Button

--- src/Context/filter_context.js
+++ src/Context/filter_context.js
@@ -6,7 +6,7 @@ const FilterContext = createContext();
 const initialState =  {
     filter_products: [],
     all_products: [],
-    grid_view: false,
+    grid_view: true,
 }
 
 export const FilterContextProvider = ({ children }) => {
@@ -19,11 +19,16 @@ export const FilterContextProvider = ({ children }) => {
     return dispatch({ type: "SET_GRID_VIEW" });
   };
 
+  // to set List view
+  const setListView = () => {
+    return dispatch({ type: "SET_LIST_VIEW" });
+  };
+
 useEffect(()=>{
   dispatch({type:"LOAD_FILTER_PRODUCTS", payload: products})
 },[products])
 
-    return <FilterContext.Provider value={{ ...state, setGridView }}>
+    return <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
         {children}
     </FilterContext.Provider>
 }
