b1302c34d33c4f904aad3957d9eed1b9f897d31e
Adarsha A <adarshaadi1997@gmail.com>
2022-11-02 12:07:28 +0530

    Search according to letters

--- src/Context/filter_context.js
+++ src/Context/filter_context.js
@@ -8,6 +8,9 @@ const initialState =  {
     all_products: [],
     grid_view: true,
     sorting_value:"lowest",
+    filters: {
+      text: "",
+    }
 }
 
 export const FilterContextProvider = ({ children }) => {
@@ -32,16 +35,26 @@ export const FilterContextProvider = ({ children }) => {
     dispatch({ type:"GET_SORT_VALUE", payload:userValue });
   };
 
+  // update the filter value
+
+  const updateFilterValue =(event)=>{
+    let name = event.target.name;
+    let value = event.target.value;
+
+    return dispatch({ type:"UPDATE_FILTER_VALUE", payload: {name, value} });
+  };
+
   // To sort the product 
   useEffect(()=>{
+    dispatch({type:"FILTER_PRODUCTS"})
     dispatch({ type:"SORTING_PRODUCTS", payload:products });
-  },[state.sorting_value])
+  },[state.sorting_value, state.filters])
 
 useEffect(()=>{
   dispatch({type:"LOAD_FILTER_PRODUCTS", payload: products})
 },[products])
 
-    return <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting }}>
+    return <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting, updateFilterValue }}>
         {children}
     </FilterContext.Provider>
 }
